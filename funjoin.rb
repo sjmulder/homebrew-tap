408bfcc6bedf00870d99a1592a05b31963568616
Sijmen J. Mulder <ik@sjmulder.nl>
2021-07-26 00:31:36 +0200

    funjoin: Update to 1.0.2

--- funjoin.rb
+++ funjoin.rb
@@ -1,16 +1,15 @@
 class Funjoin < Formula
   desc "Split separated input into files"
   homepage "https://github.com/sjmulder/funjoin"
-  url "https://github.com/sjmulder/funjoin/archive/1.0.tar.gz"
-  sha256 "80df9938f80e53566b8d5264bf5e5e83af1aa264f42431ee68e01fa81ceb95a0"
+  license "BSD-2-Clause"
+  url "https://github.com/sjmulder/funjoin/archive/1.0.2.tar.gz"
+  sha256 "3de3804281d3a41e97e015b81283d07e41c25f15e47c8a5e73388975e7b36334"
 
   def install
-    system "make"
-    bin.install "funjoin"
-    man1.install "funjoin.1"
+    system "make", "install", "PREFIX=#{prefix}", "MANPREFIX=#{prefix}/share/man"
   end
 
   test do
-    system "echo one_two | #{bin}/funjoin _ && rm xx01 xx02"
+    system "make", "check"
   end
 end
