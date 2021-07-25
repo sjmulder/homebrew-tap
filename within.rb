2bdd947b2a6bf2fd067c9cff7c9b83d340b30561
Sijmen J. Mulder <ik@sjmulder.nl>
2021-07-26 01:49:12 +0200

    within: Update to 1.1.4

--- within.rb
+++ within.rb
@@ -1,9 +1,9 @@
 class Within < Formula
   desc "Run a command in other directories"
   homepage "https://github.com/sjmulder/within"
-  license "AGPL-3.0-or-later"
-  url "https://github.com/sjmulder/within/archive/1.1.3.tar.gz"
-  sha256 "3b73b5a411407915985334eafcec00d98b469f613d25bc1af61d12f92d77403e"
+  license "BSD-2-Clause"
+  url "https://github.com/sjmulder/within/archive/1.1.4.tar.gz"
+  sha256 "e9fbf972272533a10d167a0d1b2d51dece309ebe79489b9edf72fe3adaec0f8f"
 
   def install
     system "make", "install", "PREFIX=#{prefix}", "MANPREFIX=#{prefix}/share/man"
