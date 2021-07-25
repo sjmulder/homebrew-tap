be317cb2ba830f62e77f78e969358bb1f4f4fcc8
Sijmen J. Mulder <ik@sjmulder.nl>
2021-07-25 23:53:12 +0200

    cycle: Update to 1.0.1

--- cycle.rb
+++ cycle.rb
@@ -1,13 +1,12 @@
 class Cycle < Formula
   desc "Repeat a file or stream forever"
   homepage "https://github.com/sjmulder/cycle"
-  url "https://github.com/sjmulder/cycle/archive/1.0.tar.gz"
-  sha256 "69854e1e06555c58b51df760b8e8e26429617c7b87500bb313e50e34007e4391"
+  license "BSD-2-Clause"
+  url "https://github.com/sjmulder/cycle/archive/1.0.1.tar.gz"
+  sha256 "6767402cf4d884aac1017b1c1fb7eca546fd4641fc75b9ea13aa71b5d99efd80"
 
   def install
-    system "make"
-    bin.install "cycle"
-    man1.install "cycle.1"
+    system "make", "install", "PREFIX=#{prefix}", "MANPREFIX=#{prefix}/share/man"
   end
 
   test do
