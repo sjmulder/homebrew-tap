796f44e02522ed3b9d8eb51c835a46ebc7dfd549
Sijmen J. Mulder <ik@sjmulder.nl>
2021-07-26 00:35:09 +0200

    json-yaml: Update to 1.2.1

--- json-yaml.rb
+++ json-yaml.rb
@@ -1,16 +1,15 @@
 class JsonYaml < Formula
   desc "Convert JSON to YAML"
   homepage "https://github.com/sjmulder/json-yaml"
-  url "https://github.com/sjmulder/json-yaml/archive/1.1.1.tar.gz"
-  sha256 "40d5e170ee29f3e27364581d9defd2080a4085359866e7007effae84e9111506"
+  license "BSD-2-Clause"
+  url "https://github.com/sjmulder/json-yaml/archive/1.2.1.tar.gz"
+  sha256 "b48b0fe2c806010b1ee800697974d52451da16f9645143cf8e9cfa41e4e31087"
 
   depends_on "libyaml"
   depends_on "yajl"
 
   def install
-    system "make"
-    bin.install "json-yaml"
-    man1.install "json-yaml.1"
+    system "make", "install", "PREFIX=#{prefix}", "MANPREFIX=#{prefix}/share/man"
   end
 
   test do
