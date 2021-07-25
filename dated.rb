9dd969d193e99d04c908dbd0835ba6feb79329f1
Sijmen J. Mulder <ik@sjmulder.nl>
2021-07-25 23:54:31 +0200

    dated: Update to 1.0.3

--- dated.rb
+++ dated.rb
@@ -1,16 +1,15 @@
 class Dated < Formula
   desc "Add a timestamp to every line of a stream"
   homepage "https://github.com/sjmulder/dated"
-  url "https://github.com/sjmulder/dated/archive/1.0.1.tar.gz"
-  sha256 "10d345e2f731769b77a6427810ebfe0120a67be4194c36743fbae8fb5b488889"
+  license "BSD-2-Clause"
+  url "https://github.com/sjmulder/dated/archive/1.0.3.tar.gz"
+  sha256 "846ccef8fb73ac6bfec307035d9d42c286b995343686a0cdc34493a39d2bb855"
 
   def install
-    system "make"
-    bin.install "dated"
-    man1.install "dated.1"
+    system "make", "install", "PREFIX=#{prefix}", "MANPREFIX=#{prefix}/share/man"
   end
 
   test do
-    system "echo test | #{bin}/dated"
+    system "make", "check"
   end
 end
