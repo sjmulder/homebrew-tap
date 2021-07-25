b332230638eebf743d3d6287f6b3f0b1471ad813
Sijmen J. Mulder <ik@sjmulder.nl>
2021-07-26 00:40:33 +0200

    nostt: Update to 1.2.1

--- nostt.rb
+++ nostt.rb
@@ -1,19 +1,18 @@
 class Nostt < Formula
   desc "Command-line NOS Teletekst reader"
   homepage "https://github.com/sjmulder/nostt"
-  url "https://github.com/sjmulder/nostt/releases/download/1.0/nostt-1.0.tar.gz"
-  sha256 "f7f353eb13296b5cfe030cf7fa7f1456d6293f06b061e930f2b3a6fe48a20071"
+  license "BSD-2-Clause"
+  url "https://github.com/sjmulder/nostt/archive/1.2.1.tar.gz"
+  sha256 "b002cb322b969b5c7bd6f6e86196884ee872404fece2e4339e654756c1e2daf2"
 
   depends_on "curl"
   depends_on "json-c"
 
   def install
-    system "make"
-    bin.install "nostt"
-    man1.install "nostt.1"
+    system "make", "install", "PREFIX=#{prefix}", "MANPREFIX=#{prefix}/share/man"
   end
 
   test do
-    system "#{bin}/nostt -G 100 >/dev/null"
+    system "make", "check"
   end
 end
