f9fc69313c8c0b43d02895097ca72e61839f93de
Sijmen J. Mulder <ik@sjmulder.nl>
2023-03-20 17:18:02 +0100

    nostt: Update to 1.2.2

--- nostt.rb
+++ nostt.rb
@@ -2,8 +2,8 @@ class Nostt < Formula
   desc "Command-line NOS Teletekst reader"
   homepage "https://github.com/sjmulder/nostt"
   license "BSD-2-Clause"
-  url "https://github.com/sjmulder/nostt/archive/1.2.1.tar.gz"
-  sha256 "b002cb322b969b5c7bd6f6e86196884ee872404fece2e4339e654756c1e2daf2"
+  url "https://github.com/sjmulder/nostt/archive/1.2.2.tar.gz"
+  sha256 "05b789df0ab632d5945adcaa0eb2e5b95d616b9b3f3d2eb4d71c52e3a8bb2d39"
 
   depends_on "curl"
   depends_on "json-c"
